051ecb98e5ec763e792b69342dd0bfcdc9eb3121
Michel Motta da Silva <michelmottadasilva@gmail.com>
2021-06-09 16:40:09 -0400

    first commit

---
+++ api/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'db',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'linhas-de-cuidado-db',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
---
+++ app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    Mensagem do Servidor: {{ mensagem }}
+  </div>
+</template>
+
+<script>
+
+import api from './services/api';
+import urls from './endpoints/urls';
+
+const {
+  HOME,
+} = urls;
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      mensagem: '',
+    };
+  },
+  mounted() {
+    api.get(HOME)
+      .then((response) => {
+        this.mensagem = response.data;
+      });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
